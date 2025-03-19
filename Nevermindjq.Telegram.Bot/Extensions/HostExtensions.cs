6766e2b30194ab05374705bc6d08acf6d807067b
nevermind <60211617+nevermindjq@users.noreply.github.com>
2025-03-19 22:10:03 +0300

    fix: HostExtensions.cs try to change struct

--- Nevermindjq.Telegram.Bot/Extensions/HostExtensions.cs
+++ Nevermindjq.Telegram.Bot/Extensions/HostExtensions.cs
@@ -3,20 +3,17 @@ using Nevermindjq.Models.Services.States.Abstractions;
 using Nevermindjq.Telegram.Bot.Services;
 using Nevermindjq.Telegram.Bot.Services.Hosted;
 using Nevermindjq.Telegram.Bot.States;
+using Telegram.Bot;
 
 namespace Nevermindjq.Telegram.Bot.Extensions {
 	public static class HostExtensions {
-		public static void AddTelegramBot(this IServiceCollection services, Action<FileOptions> configure) {
-			services.AddSingleton<BotStateRepository>(_ => {
-				var options = new FileOptions();
+		public static void AddTelegramBot(this IServiceCollection services, string token, FileOptions options) {
+			services.AddSingleton<ITelegramBotClient>(_ => new TelegramBotClient(token));
 
-				configure(options);
-
-				return new BotStateRepository(options);
-			});
-
+			services.AddSingleton<BotStateRepository>(_ => new BotStateRepository(options));
 			services.AddSingleton<IState<BotState>, BotStateRepository>(services => services.GetRequiredService<BotStateRepository>());
 			services.AddHostedService<BotStateRepository>(services => services.GetRequiredService<BotStateRepository>());
+
 			services.AddHostedService<Listener>();
 		}
 	}
