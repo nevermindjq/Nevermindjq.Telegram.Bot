f8784a1ca769fb66e8aa4db339f5e721dc295213
nevermind <60211617+nevermindjq@users.noreply.github.com>
2025-03-22 00:28:43 +0300

    feat: HostExtensions.cs AddCommand

--- Nevermindjq.Telegram.Bot/Extensions/HostExtensions.cs
+++ Nevermindjq.Telegram.Bot/Extensions/HostExtensions.cs
@@ -3,6 +3,9 @@ using Nevermindjq.Models.Services.States.Abstractions;
 using Nevermindjq.Telegram.Bot.Services;
 using Nevermindjq.Telegram.Bot.Services.Hosted;
 using Nevermindjq.Telegram.Bot.States;
+
+using SlimMessageBus;
+
 using Telegram.Bot;
 
 namespace Nevermindjq.Telegram.Bot.Extensions {
@@ -17,6 +20,10 @@ namespace Nevermindjq.Telegram.Bot.Extensions {
 			services.AddSingleton<UpdateDispatcher>();
 
 			services.AddHostedService<Listener>();
+		}
+
+		public static void AddCommand<TCommand>(this IServiceCollection services, string key, Func<IServiceProvider, object?, TCommand> factory) where TCommand : class, IConsumer<Update> {
+			services.AddKeyedTransient<IConsumer<Update>, TCommand>(key,  factory);
 		}
 	}
 }
