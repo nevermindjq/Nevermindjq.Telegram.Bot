9da498f1b80b50fb724633c736929d74bf195697
nevermind <60211617+nevermindjq@users.noreply.github.com>
2025-03-23 13:34:39 +0300

    chore(Nevermindjq.Telegram.Bot): Update version to 1.3.1

--- Nevermindjq.Telegram.Bot/Commands/Abstractions/IAuthenticator.cs
+++
@@ -1,7 +0,0 @@
-namespace Nevermindjq.Telegram.Bot.Commands.Abstractions {
-	public interface IAuthenticator {
-		public User? User { get; }
-
-		public Task<User?> AuthenticateAsync(Update update);
-	}
-}
