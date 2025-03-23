436b9e107c40bd9fe07abbcd92e703be22c38357
nevermind <60211617+nevermindjq@users.noreply.github.com>
2025-03-23 13:13:44 +0300

    chore: Integrated v1.3.0

--- Tests/Test_Host/Commands/MyRolesCommand.cs
+++ Tests/Test_Host/Commands/MyRolesCommand.cs
@@ -1,12 +1,14 @@
 using Bot.Data;
 
+using Nevermindjq.Telegram.Bot.Attributes;
 using Nevermindjq.Telegram.Bot.Commands.Filtered;
 
 using Telegram.Bot;
 using Telegram.Bot.Types;
 
 namespace Test_Host.Commands {
-	public class MyRolesCommand(ITelegramBotClient bot, UsersDbContext context) : MessageCommand("/roles", context) {
+	[Path("/roles"), Lifetime(ServiceLifetime.Scoped)]
+	public class MyRolesCommand(ITelegramBotClient bot, UsersDbContext context) : MessageCommand(context) {
 		public override Task ExecuteAsync(Update update) => bot.SendMessage(User.Id, string.Join('\n', User.Roles.Select(role => role.Name)));
 	}
 }
--- Tests/Test_Host/Program.cs
+++ Tests/Test_Host/Program.cs
@@ -7,20 +7,12 @@ using Microsoft.EntityFrameworkCore;
 using Nevermindjq.Telegram.Bot.Commands;
 using Nevermindjq.Telegram.Bot.Extensions;
 
-using SlimMessageBus.Host;
-using SlimMessageBus.Host.Memory;
 using Telegram.Bot;
-
-using FileOptions = Nevermindjq.Models.Services.States.Options.FileOptions;
 
 // Build
 var builder = Host.CreateApplicationBuilder(args);
 
 builder.Services.AddDbContext<UsersDbContext>(x => x.UseNpgsql(builder.Configuration.GetConnectionString("Users")), ServiceLifetime.Singleton);
-
-builder.Services.AddSlimMessageBus(config => {
-	config.WithProviderMemory().AutoRegisterCommands(Assembly.GetExecutingAssembly());
-});
 
 builder.Services.AddCommand("/start", (services, _) => new AnswerCommand {
 	Text = _ => "Some Text",
@@ -32,7 +24,7 @@ builder.Services.AddCommand("/info", (services, _) => new AnswerCommand {
 	Bot = services.GetRequiredService<ITelegramBotClient>()
 });
 
-builder.Services.AddTelegramBot(builder.Configuration["Bot:Token"]!, new FileOptions("bin/net8.0/Debug/Bot", "state"));
+builder.Services.AddTelegramBot(Assembly.GetExecutingAssembly(), builder.Configuration["Bot:Token"]!);
 
 // Run
 builder.Build().Run();
