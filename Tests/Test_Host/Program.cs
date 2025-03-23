7c25217cf0c48aaaa8ac11cf7e9cea2e228d9fa8
nevermind <60211617+nevermindjq@users.noreply.github.com>
2025-03-24 01:23:57 +0300

    test(Test_Host): Update to 1.3.5

--- Tests/Test_Host/Program.cs
+++ Tests/Test_Host/Program.cs
@@ -3,10 +3,14 @@ using System.Reflection;
 using Microsoft.EntityFrameworkCore;
 
 using Nevermindjq.Telegram.Bot.Commands;
+using Nevermindjq.Telegram.Bot.Database.Commands;
+using Nevermindjq.Telegram.Bot.Database.Commands.Abstractions;
 using Nevermindjq.Telegram.Bot.Database.Middlewares;
 using Nevermindjq.Telegram.Bot.Database.Models.Abstractions;
 using Nevermindjq.Telegram.Bot.Extensions;
 using Nevermindjq.Telegram.Bot.Middlewares.Abstractions;
+
+using Serilog;
 
 using Telegram.Bot;
 
@@ -14,6 +18,13 @@ using Test_Host.Data;
 
 // Build
 var builder = Host.CreateApplicationBuilder(args);
+
+Log.Logger = new LoggerConfiguration()
+			 .WriteTo.Console()
+			 .MinimumLevel.Verbose()
+			 .CreateLogger();
+
+builder.Services.AddSerilog(Log.Logger);
 
 builder.Services.AddDbContext<UsersDbContext>(x => x.UseNpgsql(builder.Configuration.GetConnectionString("Users")), ServiceLifetime.Singleton);
 
@@ -28,6 +39,7 @@ builder.Services.AddCommand("/info", (services, _) => new AnswerCommand {
 });
 
 builder.Services.AddScoped<ICommandMiddleware<IAuthenticatedUser>, AuthenticationMiddleware>(x => new AuthenticationMiddleware(x.GetRequiredService<UsersDbContext>()));
+builder.Services.AddScoped<IRegisterCommand, RegisterCommand>(x => new RegisterCommand(x.GetRequiredService<ITelegramBotClient>(), x.GetRequiredService<UsersDbContext>()));
 
 builder.Services.AddTelegramBot(Assembly.GetExecutingAssembly(), builder.Configuration["Bot:Token"]!);
 
